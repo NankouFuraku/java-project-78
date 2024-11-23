b58baf7687f0aff9aabfa52b1f3bb3ace372defe
Pan <toatodd@gmail.com>
2024-11-23 12:15:24 +0300

    Update MapSchema

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,10 +1,9 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class MapSchema extends BaseSchema<Map<?, ?>> {
-    private Map<String, BaseSchema<String>> nestedSchemas = new HashMap<>();
+
     public MapSchema required() {
         addRule("required", r -> r != null);
         return this;
@@ -16,28 +15,17 @@ public class MapSchema extends BaseSchema<Map<?, ?>> {
     }
 
     public void shape(Map<String, BaseSchema<String>> schemas) {
-        nestedSchemas.putAll(schemas);
-    }
+        for (var entry : schemas.entrySet()) {
+            String key = entry.getKey();
+            BaseSchema<String> schema = entry.getValue();
 
-    @Override
-    public boolean isValid(Map<?, ?> value) {
-        if (nestedSchemas.isEmpty()) {
-            return super.isValid(value);
-        }
-
-        if (!super.isValid(value)) {
-            return false;
-        }
-
-        for (var entry : value.entrySet()) {
-            String key = (String) entry.getKey();
-            if (nestedSchemas.containsKey(key)) {
-                var schema = nestedSchemas.get(key);
-                if (!schema.isValid((String) entry.getValue())) {
-                    return false;
+            addRule(key, value -> {
+                if (value.containsKey(key)) {
+                    return schema.isValid((String) value.get(key));
                 }
-            }
+
+                return true;
+            });
         }
-        return true;
     }
 }
